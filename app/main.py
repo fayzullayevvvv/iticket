1cc6264b8349235bfaa494e5d3216ac100801f42
djumanov <djumanovdev@gmail.com>
2026-04-09 18:06:47 +0500

    users model

--- app/main.py
+++ app/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
 
+from app.db.session import engine
+from app.models.base import Base
+from app.models.user import User
+
+
 app = FastAPI(title="Iticket API")
+Base.metadata.create_all(engine)
 
 
 @app.get("/")
--- app/models/user.py
+++ app/models/user.py
@@ -1,19 +1,23 @@
 import enum
 
 from sqlalchemy import Enum, Integer, String, Enum
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import Base, TimestampMixin
+
 
 class RoleEnum(str, enum.Enum):
     ADMIN = "admin"
     USER = "user"
-    
+
+
 class User(Base, TimestampMixin):
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    role: Mapped[RoleEnum] = mapped_column(Enum(RoleEnum), nullable=False, default=RoleEnum.USER)
+    role: Mapped[RoleEnum] = mapped_column(
+        Enum(RoleEnum), nullable=False, default=RoleEnum.USER
+    )
     first_name: Mapped[str] = mapped_column(String(50), nullable=False)
     last_name: Mapped[str] = mapped_column(String(50), nullable=True)
     username: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
